7219aa160675954476ceb271110d4cb2bd50f08e
Thato BENEDICT <tbenedic@e5r6p8.wethinkcode.co.za>
2018-08-20 08:10:04 +0200

    inching closer

--- push.h
+++ push.h
@@ -6,7 +6,7 @@
 /*   By: tbenedic <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/19 14:40:13 by tbenedic          #+#    #+#             */
-/*   Updated: 2018/08/19 18:21:30 by tbenedic         ###   ########.fr       */
+/*   Updated: 2018/08/20 08:09:34 by tbenedic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,4 +29,15 @@ typedef struct	s_ps
 {
 	t_stack		*stack;
 }				t_ps;
+/*
+** Prototypes for main.c
+*/
+
+/*
+** Prototypes for stack_basics.c
+*/
+ps->stack	*new_stack(t_ps	*ps, size_t size);
+int			stack_state(t_ps *ps);
+void		push(t_ps *ps);
+void		pop(t_ps *ps);
 #endif
---
+++ stack_basics.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stack_basics.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbenedic <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/20 07:35:13 by tbenedic          #+#    #+#             */
+/*   Updated: 2018/08/20 08:09:37 by tbenedic         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push.h"
+
+/*
+**   new_stack function returns;
+** - One 			if the stack is full,
+** - Negative One	if the stack is empty and
+** - Zero			otherwise.
+*/
+ps->stack		*new_stack(t_ps	*ps, size_t size)
+{
+	ps	*new_stack;
+
+	new_stack = (ps->stack *)ft_memalloc(sizeof(ps->stack));
+	ps->stack->size = size;
+	ps->stack->top = -1;
+	ps->stack->array = (int *)ft_memalloc(sizeof(int *) * ps->stack->size);
+	return (new_stack);
+}
+
+
+int		stack_state(t_ps *ps)
+{
+	if (ps->stack->top == (int)ps->stack->size - 1)
+		return (1);
+	else if (ps->stack->top == -1)
+		return (-1);
+	else
+		return (0);
+}
+
+void	push(t_ps *ps)
+{
+	if (stack_state)
+}
+
+void	pop(t_ps *ps)
+{
+}
