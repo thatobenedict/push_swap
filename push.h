ec0d8ee4cfa6f8ccd37cd104d6e49361442d011e
Thato BENEDICT <tbenedic@e4r20p4.wethinkcode.co.za>
2018-08-22 09:05:31 +0200

    we are popping and pushing boooi

--- push.h
+++ push.h
@@ -6,7 +6,7 @@
 /*   By: tbenedic <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/19 14:40:13 by tbenedic          #+#    #+#             */
-/*   Updated: 2018/08/20 08:09:34 by tbenedic         ###   ########.fr       */
+/*   Updated: 2018/08/22 09:04:57 by tbenedic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,10 +25,11 @@ typedef struct	s_stack
 	int			*array;
 }				t_stack;
 
-typedef struct	s_ps
-{
-	t_stack		*stack;
-}				t_ps;
+//typedef struct	s_ps
+//{
+//	t_stack		a;
+//	t_stack		b;
+//}				t_ps;
 /*
 ** Prototypes for main.c
 */
@@ -36,8 +37,8 @@ typedef struct	s_ps
 /*
 ** Prototypes for stack_basics.c
 */
-ps->stack	*new_stack(t_ps	*ps, size_t size);
-int			stack_state(t_ps *ps);
-void		push(t_ps *ps);
-void		pop(t_ps *ps);
+t_stack		*new_stack(size_t size);
+int			stack_state(t_stack *ps);
+void		push(t_stack *ps, int integer);
+int			pop(t_stack *ps);
 #endif
--- stack_basics.c
+++ stack_basics.c
@@ -6,43 +6,67 @@
 /*   By: tbenedic <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/20 07:35:13 by tbenedic          #+#    #+#             */
-/*   Updated: 2018/08/20 08:59:48 by tbenedic         ###   ########.fr       */
+/*   Updated: 2018/08/22 09:04:54 by tbenedic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push.h"
+#include <stdio.h>
 
-ps->stack		*new_stack(t_ps	*ps, size_t size)
+t_stack		*new_stack(size_t size)
 {
-	ps	*new_stack;
-
-	new_stack = (ps->stack *)ft_memalloc(sizeof(ps->stack));
-	ps->stack->size = size;
-	ps->stack->top = -1;
-	ps->stack->array = (int *)ft_memalloc(sizeof(int *) * ps->stack->size);
-	return (new_stack);
+	t_stack *ps = (t_stack*)ft_memalloc(sizeof(t_stack));
+	ps->size = size;
+	ps->top = -1;
+	ps->array = (int *)ft_memalloc(sizeof(int *) * ps->size);
+	return (ps); 
 }
 /*
-**   new_stack function returns;
-** - One 			if the stack is full,
-** - Negative One	if the stack is empty and
-** - Zero			otherwise.
-*/
-int		stack_state(t_ps *ps)
+ **   new_stack function returns;
+ ** - One 			if the stack is full,
+ ** - Negative One	if the stack is empty and
+ ** - Zero			otherwise.
+ */
+int		stack_state(t_stack *ps)
 {
-	if (ps->stack->top == (int)ps->stack->size - 1)
+	if (ps->top == (int)(ps->size - 1))
 		return (1);
-	else if (ps->stack->top == -1)
+	else if (ps->top == -1)
 		return (-1);
 	else
 		return (0);
 }
-/*
-void	push(t_ps *ps)
+
+void	push(t_stack *ps, int integer)
 {
-	if (stack_state(ps->stack) == )
+	if (stack_state(ps) == 1)
+		return ;
+	ps->array[++ps->top] = integer;
 }
 
-void	pop(t_ps *ps)
+int		pop(t_stack *ps)
 {
-}*/
+	if (stack_state(ps) == -1)
+		return (-1);
+	return (ps->array[ps->top--]);
+}
+// TESTER
+int main()
+{
+	t_stack  *ps;
+	ps = NULL;
+//	ps->top = 0;
+//	ps->size = 0;
+//	ps->array = 0;
+	t_stack *a;
+	a = new_stack(input;
+	b = new_stack(input)	
+
+	push(a, 10);
+	push(a, 20);
+	push(a, 30);
+
+	printf("%d popped from stack\n", pop(a));
+
+	return 0;
+}
