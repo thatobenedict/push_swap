a02408655123f2bd569f2b5c6374c4d3859f0683
Thato BENEDICT <tbenedic@e4r20p4.wethinkcode.co.za>
2018-09-10 18:50:15 +0200

    almost there - there is an edge case not being catered for you need to find

--- algo3.c
+++ algo3.c
@@ -6,7 +6,7 @@
 /*   By: tbenedic <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/08 10:43:47 by tbenedic          #+#    #+#             */
-/*   Updated: 2018/09/09 12:19:10 by tbenedic         ###   ########.fr       */
+/*   Updated: 2018/09/10 18:49:37 by tbenedic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,53 +15,34 @@
 void	hard_sort2_b(t_ps *ps)
 {
 	if (check_sort_stack(ps->b) == 0)
-		do_sb(ps);
+		do_rb(ps);
 	return ;
 }
 
 int		ab_score(int i, t_stack *stack)
 {
-	int score;
+	//	int score;
 	int diff;
 
-	score = 0;
+	//	score = 0;
 	if (i > stack->top / 2)
 		diff = stack->top - i;
 	else
-		diff = i + 1;
-	while (--diff >= 0)
-	{
-		if (i > stack->top / 2)
-			score++;
-		else
-			score--;
-	}
-	return (score);
+		diff = (-1)*(i + 1);
+	//	while (--diff >= 0) 				TRYING THIS 10/09/2019
+	//	{
+	//		if (i > stack->top / 2)
+	//			score++;
+	//		else
+	//			score--;
+	//	}
+	//	return (score);
+	return (diff);	
 }
-/*
-int		rot_score(t_stack *stack)
-{
-	int score;
-	int diff;
 
-	score = 0;
-	if (ret_index(who_min(stack), stack) > stack->top / 2)
-		diff = stack->top - ret_index(who_min(stack), stack);
-	else
-		diff = ret_index(who_min(stack), stack) + 1;
-	while (--diff >= 0)
-	{
-		if (ret_index(who_min(stack), stack) > stack->top / 2)
-			score++;
-		else
-			score--;
-	}
-	return (score);
-}
-*/
 /*
-** =This function seeks to find out the true number of rotations required to achieve the correct pb
-*/
+ ** =This function seeks to find out the true number of rotations required to achieve the correct pb
+ */
 int		real_rot(int a, int b)
 {
 	if (a < 0 && b < 0)
@@ -74,14 +55,14 @@ int		real_rot(int a, int b)
 
 int		is_between(int a, int b, int c)
 {
-	if (a > b)
+	if (a < b)
 	{
-		if (b > c && a < c)
+		if (a < c && b > c)
 			return (0);
 	}
 	else
 	{
-		if (a > c && b < c)
+		if (b < c && a > c)
 			return (0);
 	}
 	return (1);
@@ -89,25 +70,25 @@ int		is_between(int a, int b, int c)
 
 int		is_valid(int i, int j, t_ps *ps)
 {
-		if (i == ps->b->top && who_max(ps->b) == ps->b->array[i])
-		{
-			if (ps->b->array[i] < ps->a->array[j] ||
-					ps->b->array[i] > ps->a->array[j])
-				return (0);
-		}
-		else if (i == ps->b->top)
-		{
-			if (is_between(ps->b->array[i], ps->b->array[0],
-						ps->a->array[j]) == 0)
-				return (0);
-		}
-		else
-		{
-			if (is_between(ps->b->array[i], ps->b->array[i + 1],
-						ps->a->array[j]) == 0)
-				return (0);
-		}
-		return (1);
+	if (i == ps->b->top && who_max(ps->b) == ps->b->array[i])
+	{
+		if (ps->b->array[i] > ps->a->array[j] ||
+			ps->b->array[i] < ps->a->array[j])
+			return (0);
+	}
+	else if (i == ps->b->top)
+	{
+		if (is_between(ps->b->array[i], ps->b->array[0],
+					ps->a->array[j]) == 0)
+			return (0);
+	}
+	else
+	{
+		if (is_between(ps->b->array[i], ps->b->array[i + 1],
+					ps->a->array[j]) == 0)
+			return (0);
+	}
+	return (1);
 }
 
 void	store_top(int a, int b, t_ps *ps)
@@ -122,12 +103,14 @@ void	store_top(int a, int b, t_ps *ps)
 			ft_abs(b) - ft_abs(a) : 0;
 		ps->score.dir_a = -1;
 		ps->score.dir_b = -1;
+	//	ft_putstr("###########STORE TOP 1\n");
 	}
 	else if (a > 0 && b > 0)
 	{
 		ps->score.mag = ft_ismin(ft_abs(a),ft_abs(b));
 		ps->score.mag_a = (a > b) ? a - b : 0;
 		ps->score.mag_b = (b > a) ? b - a : 0;
+	//	ft_putstr("###########STORE TOP 2\n");
 	}
 	else
 	{
@@ -135,6 +118,7 @@ void	store_top(int a, int b, t_ps *ps)
 		ps->score.mag_b = ft_abs(b);
 		ps->score.dir_a = (a < 0) ? -1 : 0;
 		ps->score.dir_b = (b < 0) ? -1 : 0;
+	//	ft_putstr("###########STORE TOP 3\n");
 	}
 }
 
@@ -162,30 +146,51 @@ void	sort_engine(t_ps *ps)
 	int j;
 	int score;
 
+	score = 0;
 	//could send these to dynamic sort later
-	ps->score.top = 10000;	
-	ps->score.mag_a = 0;	
-	ps->score.mag_b = 0;	
-	ps->score.dir_a = 0;	
-	ps->score.dir_b = 0;	
-	ps->score.mag = 0;	
-	ps->score.dir = 0;	
+//	ps->score.top = 10000;	
+//	ps->score.mag_a = 0;	
+//	ps->score.mag_b = 0;	
+//	ps->score.dir_a = 0;	
+//	ps->score.dir_b = 0;	
+//	ps->score.mag = 0;	
+//	ps->score.dir = 0;	
 	i = ps->b->top + 1;
 	while (--i >= 0)
 	{
+	//	ft_putstr("HERE WE ARE 1\n");
 		j = ps->a->top + 1;
 		while (--j >= 0)
 		{
+	//		ft_putstr("HERE WE ARE 2\n");
 			if (is_valid(i, j, ps) == 0)
 			{
+	//			ft_putstr("HERE WE ARE 3\n");
 				score = real_rot(ab_score(j, ps->a), ab_score(i, ps->b));
+				ft_putstr("VAR A ");
+				ft_putnbr(ps->a->array[j]);
+				ft_putchar('\n');
+				ft_putstr("SCORE A\n");
+				ft_putnbr(ab_score(j, ps->a));
+				ft_putchar('\n');
+				ft_putstr("VAR B ");
+				ft_putnbr(ps->b->array[i]);
+				ft_putchar('\n');
+				ft_putstr("SCORE B\n");
+				ft_putnbr(ab_score(i, ps->b));
+				ft_putchar('\n');
 				if (score < ps->score.top)
 				{
 					store_top(ab_score(j, ps->a), ab_score(i, ps->b), ps);
 					ps->score.top = score;
+					ft_putstr("$$$$$$$$$$$$$$$$$$$$$$$TOP SCORE\n");
+					ft_putnbr(ps->score.top);
+					ft_putchar('\n');
+					display_stack(ps->a, ps->b);
+
 				}
 			}			
 		}
 	}
-	return ;	
+	//	return ;	
 }
--- main_push.c
+++ main_push.c
@@ -6,137 +6,43 @@
 /*   By: tbenedic <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/30 12:44:05 by tbenedic          #+#    #+#             */
-/*   Updated: 2018/09/09 12:19:12 by tbenedic         ###   ########.fr       */
+/*   Updated: 2018/09/10 18:49:40 by tbenedic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push.h"
 
-/*
- **void	make_play(int j, int k, t_ps *ps)
- **{
- **	if (ps->top.dir_a == 1 && ps->top.dir_b == 1)
- **	{
- **		while (++j < ps->top.mag_a && ++k < ps->top.mag_b)
- **			ft_rr(ps);
- **	}
- **	if (ps->top.dir_a == 0 && ps->top.dir_b == 0)
- **	{
- **		while (++j < ps->top.mag_a && ++k < ps->top.mag_b)
- **			ft_rrr(ps);
- **	}
- **	if (ps->top.dir_a == 0)
- **	{
- **		while (++j < ps->top.mag_a)
- **			ft_rra(ps);
- **	}
- **	else if (ps->top.dir_a == 1)
- **	{
- **		while (++j < ps->top.mag_a)
- **			ft_ra(ps);
- **	}
- **	if (ps->top.dir_b == 0)
- **	{
- **		while (++j < ps->top.mag_b)
- **			ft_rrb(ps);
- **	}
- **	else if (ps->top.dir_b == 1)
- **	{
- **		while (++j < ps->top.mag_b)
- **			ft_rb(ps);
- **	}
- **}
- **
- **void	print_play(int j, int k, t_ps *ps)
- **{
- **	if (ps->top.dir_a == 1 && ps->top.dir_b == 1)
- **	{
- **		while (++j < ps->top.mag_a && ++k < ps->top.mag_b)
- **			ft_putstr_fd("rr\n", 2);
- **	}
- **	if (ps->top.dir_a == 0 && ps->top.dir_b == 0)
- **	{
- **		while (++j < ps->top.mag_a && ++k < ps->top.mag_b)
- **			ft_putstr_fd("rrr\n", 2);
- **	}
- **	if (ps->top.dir_a == 0)
- **	{
- **		while (++j < ps->top.mag_a)
- **			ft_putstr_fd("rra\n", 2);
- **	}
- **	else if (ps->top.dir_a == 1)
- **	{
- **		while (++j < ps->top.mag_a)
- **			ft_putstr_fd("ra\n", 2);
- **	}
- **	if (ps->top.dir_b == 0)
- **	{
- **		while (++j < ps->top.mag_b)
- **			ft_putstr_fd("rrb\n", 2);
- **	}
- **	else if (ps->top.dir_b == 1)
- **	{
- **		while (++j < ps->top.mag_b)
- **			ft_putstr_fd("rb\n", 2);
- **	}
- **}
- **
- **void	push_creator(t_ps *ps, int ac, char **av)
- **{
-**	int i;
-**	int j;
-**	int k;
-**
-**	i = ac - ps->trig;
-**	while (i >= 0)
-	**	{
-		**		push(ps->a, ft_atoi(av[i]));
-		**		i--;
-		**	}
-		**	ft_pb(ps);
-		**	ft_putstr_fd("pb\n", 2);
-		**	ft_pb(ps);
-		**	ft_putstr_fd("pb\n", 2);
-		**	display_stack(ps->a, ps->b);
-**	while (check_sort_int(ps) == 1)
-	**	{
-		**		j = -1;
-		**		k = -1;
-		**		ps->top.dir_a = 0;
-		**		ps->top.dir_b = 0;
-		**		ps->top.mag_a = 0;
-		**		ps->top.mag_b = 0;
-		**		display_stack(ps->a, ps->b);
-		**		valid_rot(ps);
-		**		make_play(j, k, ps);
-		**		print_play(j, k, ps);
-		**		ft_pb(ps);
-		**		ft_putstr_fd("pb\n", 2);
-		**		display_stack(ps->a, ps->b);
-		**	}
-		**}
-		*/
-
 void	dynamic_sort(t_ps *ps)
 {
-	//	ft_putnbr(ps->score.mag);
-	//	ft_putchar('\n');
-	//		ft_putnbr(ps->score.mag_a);
-	//		ft_putchar('\n');
-	//		ft_putnbr(ps->score.mag_b);
-	//		ft_putchar('\n');
-	//	ft_putnbr()
-
 	do_pb(ps);
 	do_pb(ps);
 
 	hard_sort2_b(ps);
-	ft_putstr("here 1\n");         //go together
-	display_stack(ps->a, ps->b);   // go together
-	int i = ps->a->top;
-	while (i >= 0)
+	display_stack(ps->a, ps->b);   // delete me
+	while (ps->a->top >= 0)
 	{
 		sort_engine(ps);
+		ft_putstr("ps->score.top\n");
+		ft_putnbr(ps->score.top);
+		ft_putchar('\n');
+		ft_putstr("ps->score.mag_a\n");
+		ft_putnbr(ps->score.mag_a);
+		ft_putchar('\n');
+		ft_putstr("ps->score.mag_b\n");
+		ft_putnbr(ps->score.mag_b);
+		ft_putchar('\n');
+		ft_putstr("ps->score.dir_a\n");
+		ft_putnbr(ps->score.dir_a);
+		ft_putchar('\n');
+		ft_putstr("ps->score.dir_b\n");
+		ft_putnbr(ps->score.dir_b);
+		ft_putchar('\n');
+		ft_putstr("ps->score.mag\n");
+		ft_putnbr(ps->score.mag);
+		ft_putchar('\n');
+		ft_putstr("ps->score.dir\n");
+		ft_putnbr(ps->score.dir);
+		ft_putchar('\n');
 
 		while (ps->score.mag > 0)
 		{
@@ -145,27 +51,39 @@ void	dynamic_sort(t_ps *ps)
 			else
 				do_rrr(ps);
 			ps->score.mag--;
+			display_stack(ps->a, ps->b);   // delete me
 		}
 		while (ps->score.mag_a > 0)
 		{
-			if (ps->score.dir == 0)
+			if (ps->score.dir_a == 0)
 				do_ra(ps);
 			else
 				do_rra(ps);
 			ps->score.mag_a--;
+			display_stack(ps->a, ps->b);   // delete me
 		}
 		while (ps->score.mag_b > 0)
 		{
-			if (ps->score.dir == 0)
+			if (ps->score.dir_b == 0)
 				do_rb(ps);
 			else
 				do_rrb(ps);
 			ps->score.mag_b--;
+			display_stack(ps->a, ps->b);   // delete me
 		}
 		do_pb(ps);
-		i--;
+		display_stack(ps->a, ps->b);   // delete me
+	
+		ps->score.top = 10000;
+	ps->score.mag_a = 0;
+	ps->score.mag_b = 0;
+	ps->score.dir_a = 0;
+	ps->score.dir_b = 0;
+	ps->score.mag = 0;
+	ps->score.dir = 0;
 	}
-	b_max_to_top(ps);
+	b_max_to_top(ps); //								 $$$ WORKING LIKE A GEM
+	display_stack(ps->a, ps->b);   // go together
 	while (ps->b->top >= 0)
 		do_pa(ps);
 	display_stack(ps->a, ps->b);   // go together
